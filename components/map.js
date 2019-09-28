c575d8575b69752abab1b7d8a86a9601f74b47e4
Matt Ilagan <matt.r.ilagan@gmail.com>
2019-09-28 16:51:47 -0700

    Add API for geojson retrieval

--- components/map.js
+++ components/map.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
 import MapView, {MarkerAnimated, Overlay} from "react-native-maps";
 import Geojson from 'react-native-geojson'
-const intersectionGeo = require('./intersections.json');
-const walksideGeo = require('./walkside.json');
 
 const styles = StyleSheet.create({
   page: {
@@ -29,16 +27,54 @@ export default class Map extends Component {
       longitude: -123.120735,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
+    },
+    intersections: {},
+    walkside: {},
+    viewMap: false
+  }
+  
+  componentDidMount(){
+    fetch("https://walkside-api.herokuapp.com/intersections")
+    .then(response => response.json())
+    .then((responseJson)=> {
+      this.setState({
+       intersections: responseJson
+      })
+    })
+    .catch(error=>console.log(error)) //to catch the errors if any
+
+    fetch("https://walkside-api.herokuapp.com/walkside")
+    .then(response => response.json())
+    .then((responseJson)=> {
+      this.setState({
+       walkside: responseJson
+      })
+      this.readyToggle()
+    })
+    .catch(error=>console.log(error)) //to catch the errors if any
+  }
+
+  readyToggle = () => {
+    if(this.state.viewMap === false){
+      this.setState({viewMap: true})
     }
   }
 
   render() {
     return (
-      <MapView style={styles.map}
+      this.state.viewMap &&<MapView style={styles.map}
         region={this.state.region}
+        zoomControlEnabled={true}
+        zoomEnabled={true}
+        zoomTapEnabled={true}
         >
-          <Geojson geojson={intersectionGeo} />
-          <Geojson geojson={walksideGeo} />
+          <MarkerAnimated
+            coordinate={this.state.region}
+            title="Me"
+            description="Represents you"
+          />
+          {/* <Geojson geojson={intersectionGeo} /> */}
+          <Geojson geojson={this.state.walkside} />
       </MapView>
     );
   }
